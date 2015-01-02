34fcfe9634ff8870f5cdf0131f40f1caea968c04
Andrew Whitaker <whitaker@cs.washington.edu>
2015-01-02 10:35:26 -0800

    Rename basic_tests to infrastructure_tests

--- pyrlog/test/infrastructure_tests.py
+++ pyrlog/test/infrastructure_tests.py
@@ -9,6 +9,11 @@ import gevent
 
 """Tests of the simulation infrastructure."""
 
+def run_instance(node, func):
+    """Execute a client or server function; exit on termination."""
+    func(node)
+    node.exit()
+
 class InfrastructureTests(unittest.TestCase):
     def setUp(self):
         self.clock = FakeClock()
@@ -17,6 +22,12 @@ class InfrastructureTests(unittest.TestCase):
         self.nodes = [FakeNode(i, self.network, self.clock) for i in range(2)]
         self.server = self.nodes[0]
         self.client = self.nodes[1]
+
+    def __run(self, server_run, client_run):
+        gevent.joinall([
+            gevent.spawn(run_instance, self.server, server_run),
+            gevent.spawn(run_instance, self.client, client_run)
+            ], raise_error=True)
 
     def test_client_server(self):
         def server_run(node):
@@ -44,7 +55,18 @@ class InfrastructureTests(unittest.TestCase):
 
             print 'CLIENT BYE'
 
-        gevent.joinall([
-            gevent.spawn(server_run, self.server),
-            gevent.spawn(client_run, self.client)
-            ], raise_error=True)
+        self.__run(server_run, client_run)
+
+    def test_timeout(self):
+        def run(node):
+            with self.assertRaises(NodeTimeout):
+                src, message = node.receive(block=False, timeout=100)
+
+        self.__run(run, run)
+
+    def test_deadlock(self):
+        def run(node):
+            src, msg = node.receive(block=True)
+
+        with self.assertRaises(gevent.hub.LoopExit):
+            self.__run(run, run)
